4a65afc59514c04d44b6de8874bbc0def75d1b18
Robiul Awal <robiulawal527@gmail.com>
2023-06-22 01:29:24 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function submitForm() {
     
     // Example: Display a sample prescription result
     var prescriptionResult = "Prescription: Take ibuprofen 200mg twice a day for 3 days.";
+    var prescriptionResult = " Take ibuprofen 20mg twice a day for 5 days.";
     document.getElementById("prescriptionResult").textContent = prescriptionResult;
   }
   
