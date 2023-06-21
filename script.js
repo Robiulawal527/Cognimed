e08915d91d70804b4bb514ed7ca80bd340162f1c
Robiul Awal <robiulawal527@gmail.com>
2023-06-22 01:13:53 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function submitForm() {
+    var userInput = document.getElementById("userInput").value;
+  
+    // Perform any necessary processing or API calls with the user input
+    
+    // Example: Display a sample prescription result
+    var prescriptionResult = "Prescription: Take ibuprofen 200mg twice a day for 3 days.";
+    document.getElementById("prescriptionResult").textContent = prescriptionResult;
+  }
+  
